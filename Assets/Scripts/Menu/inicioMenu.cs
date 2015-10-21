c6c04dc2f105a84ff8f145be0c25501fa580691d
mathino <mathsouza64@gmail.com>
2015-10-21 00:04:38 -0200

    C# animação

---
+++ Assets/Scripts/Menu/inicioMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inicioMenu : MonoBehaviour {
+
+    private SpriteRenderer sR;
+    private bool stopMenuAnim;
+
+    // Use this for initialization
+	void Start () {
+        sR = this.GetComponent<SpriteRenderer>();
+        stopMenuAnim = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(sR.sprite.name == "Menu0056")
+        {
+            stopMenuAnim = true;
+        }
+        makeMenu();
+	}
+
+    void makeMenu() 
+    {
+        if (stopMenuAnim)
+            this.GetComponent<Animator>().SetBool("toStopMenuAnim", true);
+        else
+            this.GetComponent<Animator>().SetBool("toStopMenuAnim", false);
+    }
+}
