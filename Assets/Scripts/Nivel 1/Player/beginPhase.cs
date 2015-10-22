f3948dafe6d3e7a75ade3e93f1db891128a66286
mathino <mathsouza64@gmail.com>
2015-10-22 21:20:29 -0200

    add animação na entrada

--- Assets/Scripts/Nivel 1/Player/beginPhase.cs
+++ Assets/Scripts/Nivel 1/Player/beginPhase.cs
@@ -12,34 +12,52 @@ public class beginPhase : MonoBehaviour {
 
 	public bool arrivedToGatilho8;
 	public bool arrivedToGatilho9;
+    public bool stopAnimation;
 
 	void Start() {
 		
 		pm = this.GetComponent<Player_Mov>();
 	}
 
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        stopAnimation = (col.tag == "gatilho9");
+    }
+
 	void Update () {
 
 		if (!begin && !arrivedToGatilho8) {
 			transform.position = Vector3.MoveTowards (transform.position, gatilho8.transform.position, Time.deltaTime * 4.3597135345f);
+            this.GetComponent<Animator>().SetBool("toMove", true);
 			pm.ray = gatilho8.transform.position;
 		}
 
-		if (!begin && !arrivedToGatilho8 && transform.position == gatilho8.transform.position) 
-			arrivedToGatilho8 = true;
+        if (!begin && !arrivedToGatilho8 && transform.position == gatilho8.transform.position)
+        {
+           
+            arrivedToGatilho8 = true;
+        }
 		
 		if (!begin && arrivedToGatilho8 && !arrivedToGatilho9) {
 			transform.position = Vector3.MoveTowards (transform.position, gatilho9.transform.position, Time.deltaTime * 4.3597135345f);
+            this.GetComponent<Animator>().SetBool("toMove", true);
 			pm.ray = gatilho9.transform.position;
 		}
 
-		if (!begin && !arrivedToGatilho9 && transform.position == gatilho9.transform.position)  
-			arrivedToGatilho9 = true;
+        if (!begin && !arrivedToGatilho9 && transform.position == gatilho9.transform.position){
+            arrivedToGatilho9 = true;
+            
+        }
 		
 		if (!begin && arrivedToGatilho9) {
 			transform.position = Vector3.MoveTowards (transform.position, gatilho10.transform.position, Time.deltaTime * 4.3597135345f);
+            
 			pm.ray = gatilho10.transform.position;
 		}
+        if (stopAnimation)
+        {
+            this.GetComponent<Animator>().SetBool("toMove", false);
+        }
 
 	}
 }
