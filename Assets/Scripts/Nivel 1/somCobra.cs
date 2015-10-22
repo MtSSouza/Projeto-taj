ed74f702dc57e749eb9beb50a55958d57303d7b7
mathino <mathsouza64@gmail.com>
2015-10-22 12:18:45 -0200

    concerto do som da combra

--- Assets/Scripts/Nivel 1/somCobra.cs
+++ Assets/Scripts/Nivel 1/somCobra.cs
@@ -6,20 +6,22 @@ public class somCobra : MonoBehaviour {
 	private AudioSource aS;
 	private TriggerFind cA;
 	public GameObject aC;
+	public bool activSomFlauta;
+
 
 	// Use this for initialization
 	void Start () {
 		aS = this.GetComponent<AudioSource> ();
 		cA = this.gameObject.GetComponent("TriggerFind") as TriggerFind;
-
+		activSomFlauta = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		Debug.Log (aS.audio.isPlaying);
 		if (aC.gameObject.activeSelf == true && cA.canGoUp == true) {
 			aS.audio.mute = false;
+
 		} else {
 			aS.audio.mute = true;
 		}
