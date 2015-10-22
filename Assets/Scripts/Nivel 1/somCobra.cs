53fbbfa656c11db4576ea6ef6dcdf1550d002170
mathino <mathsouza64@gmail.com>
2015-10-22 10:08:19 -0200

    adcionado som de cobra ao jogo

---
+++ Assets/Scripts/Nivel 1/somCobra.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class somCobra : MonoBehaviour {
+
+	private AudioSource aS;
+	private TriggerFind cA;
+	public GameObject aC;
+
+	// Use this for initialization
+	void Start () {
+		aS = this.GetComponent<AudioSource> ();
+		cA = this.gameObject.GetComponent("TriggerFind") as TriggerFind;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log (aS.audio.isPlaying);
+		if (aC.gameObject.activeSelf == true && cA.canGoUp == true) {
+			aS.audio.mute = false;
+		} else {
+			aS.audio.mute = true;
+		}
+	}
+}
