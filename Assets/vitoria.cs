145f77f64c16f4fbc182d2b93e184039868560b9
mathino <mathsouza64@gmail.com>
2015-10-23 11:24:32 -0200

    Logica de vitoria

---
+++ Assets/vitoria.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vitoria : MonoBehaviour {
+
+	public GameObject sWG;
+	private ShowWish sW;
+	private SpriteRenderer sR;
+
+	// Use this for initialization
+	void Start () {
+		sW = sWG.gameObject.GetComponent ("ShowWish") as ShowWish;
+		sR = this.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (sW.win) {
+			this.gameObject.SetActive (true);
+			this.GetComponent<Animator> ().SetBool ("isWin", true);
+		} else {
+			this.gameObject.SetActive (false);
+			this.GetComponent<Animator> ().SetBool ("isWin", false);
+		}
+
+		if (sR.sprite.name == "") {
+			this.GetComponent<Animator> ().SetBool ("isWin", false);
+		}
+	}
+}
